8867674eadc13e809434d1c84a2ded48f34467b3
pawan335 <pawan8090335@gmail.com>
2021-01-10 15:15:05 +0530

    broken home page in component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Directory from '../../components/directory-menu/directory-menu.component'
+import './homepage.style.scss';
+
+
+export const HomePage=()=>(
+    <div className='homepage'>
+      <Directory />
+    </div>
+);
+export default HomePage;
